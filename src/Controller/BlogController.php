f553b03e2502d9ef2f7b5e9b1c754b9f9b315700
laille2 <louis.laillet@gmail.com>
2021-07-21 11:14:38 +0200

    Ajout des formulaires Articles et Commentaires

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use App\Form\ArticleType;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -67,13 +69,43 @@ class BlogController extends AbstractController
     }
 
     /**
-     * @Route("/blog/{id}", name="blog_show")
+     * @Route("/blog/{id_article}", name="blog_show")
+     * @Route("/blog/{id_article}/modify_comment/{id_comment}", name="blog_modify_comment")
      */
-    public function show(Article $article)
+    public function commentForm($id_article, int $id_comment = null, Request $request, EntityManagerInterface $manager)
     {
+        $article = $manager->getRepository(Article::class)->find($id_article);
+        $comment = new Comment();
+        
+        if ($id_comment) {
+            $comment = $manager->getRepository(Comment::class)->find($id_comment);
+        }
+
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$comment->getId()) {
+                $comment->setCreatedAt(new \DateTimeImmutable());
+                $comment->setArticle($article);
+            }
+
+            $article->addComment($comment);
+
+            $manager->persist($comment);
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', [
+                'id_article' => $id_article
+            ]);
+        }
 
         return $this->render('blog/show.html.twig', [
-            'article' => $article
+            'article' => $article,
+            'formComment' =>  $form->createView(),
+            'editMode' => $comment->getId() !== null
         ]);
     }
 }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,16 +12,50 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i <= 10; $i++) {
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("http://via.placeholder.com/350x150")
-                    ->setCreatedAt(new \DateTimeImmutable());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($article);
+        // Créer 3 catégories fakées
+        for ($i = 1; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+
+            // Créer entre 4 et 6 articles fakées
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+
+                $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt(new \DateTimeImmutable($faker->dateTimeBetween()->format('Y-m-d H:i:s')))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                // Commentaires de l'article
+                for ($k = 0; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+
+                    $content = '<p>' . join('</p><p>', $faker->paragraphs(2)) . '</p>';
+
+
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content)
+                        ->setCreatedAt(new \DateTimeImmutable($faker->dateTimeBetween('-' . $days . ' days')->format('Y-m-d H:i:s')))
+                        ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+                
+            }
         }
-
         $manager->flush();
     }
 }
